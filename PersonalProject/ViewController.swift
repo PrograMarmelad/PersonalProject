b063b2bac11b191fc3aa376e6a8629ce14453de4
Valera Zhdanov <valerazhdanov@MacBook-Air-Valera.local>
2024-10-06 21:16:07 +0500

    4 урок

--- PersonalProject/ViewController.swift
+++ PersonalProject/ViewController.swift
@@ -9,9 +9,15 @@ import UIKit
 
 class ViewController: UIViewController {
 let name = "Валерий"
+let surName = "Жданов"
+    var fullName: String {
+        "\(name) \(surName)"
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         print("Меня зовут \(name), я создал своё первое приложение")
+        print(fullName)
         // Do any additional setup after loading the view.
     }
 
