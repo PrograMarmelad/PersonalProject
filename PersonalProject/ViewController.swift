93fd849fa25734ad216341c403d21953c7ace42b
Valera Zhdanov <valerazhdanov@MacBook-Air-Valera.local>
2024-10-06 20:59:46 +0500

    3 урок

---
+++ PersonalProject/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  PersonalProject
+//
+//  Created by Valera Zhdanov on 06.10.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+let name = "Валерий"
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        print("Меня зовут \(name), я создал своё первое приложение")
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
