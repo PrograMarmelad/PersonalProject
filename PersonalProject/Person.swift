868fbe3b514998e1368301cd15751695d739b962
PrograMarmelad <valera_zhdanov_2000@mail.ru>
2024-10-10 21:32:48 +0500

    Add class Person

---
+++ PersonalProject/Person.swift
@@ -0,0 +1,16 @@
+//
+//  Person.swift
+//  PersonalProject
+//
+//  Created by Valera Zhdanov on 10.10.2024.
+//
+
+import Foundation
+
+class Person {
+    let name = "Валерий"
+    let surName = "Жданов"
+    var fullName: String {
+        "\(name) \(surName)"
+    }
+}
--- PersonalProject/ViewController.swift
+++ PersonalProject/ViewController.swift
@@ -8,19 +8,13 @@
 import UIKit
 
 class ViewController: UIViewController {
-let name = "Валерий"
-let surName = "Жданов"
-    var fullName: String {
-        "\(name) \(surName)"
-    }
-    
     override func viewDidLoad() {
         super.viewDidLoad()
-        print("Меня зовут \(name), я создал своё первое приложение")
-        print(fullName)
+        
+        let person = Person()
+        print("Меня зовут \(person.name), я создал своё первое приложение")
+        print(person.fullName)
         // Do any additional setup after loading the view.
     }
-
-
 }
 
