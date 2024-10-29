162ff25960ddc2e745406a23e7b062eff16d541c
PrograMarmelad <valera_zhdanov_2000@mail.ru>
2024-10-30 02:31:41 +0500

    Creat UILabel, random output User

--- PersonalProject/Helper.swift
+++ PersonalProject/Helper.swift
@@ -7,7 +7,40 @@ class Helper {
         user.append(people)
     }
     
+    func addUsers(_ peoples: User...) {
+        user.append(contentsOf: peoples)
+    }
+    
     func getUser() -> [User] {
         user
+    }
+}
+
+class UserRepository {
+    func getUser() -> [User] {
+        
+        let personOne = Person(
+            name: "Валерий",
+            surName: "Жданов"
+        )
+        
+        let personTwo = Person(
+            name: "Виктория",
+            surName: "Жданова"
+        )
+        
+        let userOne = User(
+            login: "Valera",
+            password: 222,
+            personInform: personOne
+        )
+        
+        let userTwo = User(
+            login: "Vika",
+            password: 333,
+            personInform: personTwo
+        )
+        
+        return ([userOne, userTwo])
     }
 }
--- PersonalProject/ViewController.swift
+++ PersonalProject/ViewController.swift
@@ -4,13 +4,17 @@ class ViewController: UIViewController {
     
     private let helper = Helper()
     private let userRepository = UserRepository()
+    private let fullNameLabel = UILabel()
     
     override func viewDidLoad() {
         
         super.viewDidLoad()
         view.backgroundColor = .cyan
-        view.alpha = 0.1
+        view.alpha = 0.7
         updatePerson()
+        
+        setupLabel()
+        view.addSubview(fullNameLabel)
     }
     
     private func updatePerson() {
@@ -21,6 +25,14 @@ class ViewController: UIViewController {
         for user in helper.getUser() {
             print(user.personInform.fullName)
         }
+    }
+    
+    private func setupLabel() {
+        let randomFullName = userRepository.getUser().randomElement()
+        fullNameLabel.text = randomFullName?.personInform.fullName
+        fullNameLabel.font = .systemFont(ofSize: 25)
+        fullNameLabel.textColor = .blue
+        fullNameLabel.frame = CGRect(x: 100, y: 100, width: 100, height: 50)
     }
 }
 
